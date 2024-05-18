251f0124ca8c372bd2960e2a8bd956e63cee2d2c
Marco Driessen <marco.driessen@elements.nl>
2024-05-18 15:11:29 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Marco Driessen on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
